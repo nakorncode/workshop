24b6c1fba61e5670c557b095bc10f0a5cace56ce
Nakorn Sinpadung <nakorndev@gmail.com>
2025-03-02 21:10:27 +0700

    feat: add Vue lifecycle examples with Basic and Practical components

---
+++ vue/lifecycle/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Basic from './components/Basic.vue'
+import Practical from './components/Practical.vue'
+import { ref } from 'vue'
+
+const showBasic = ref(true)
+const showPractical = ref(true)
+</script>
+
+<template>
+  <Basic v-if="showBasic"></Basic>
+
+  <hr>
+
+  <button @click="showBasic = !showBasic">{{ showBasic ? 'Hide Basic' : 'Show Basic' }}</button>
+
+  <hr>
+
+  <Practical v-if="showPractical"></Practical>
+
+  <hr>
+
+  <button @click="showPractical = !showPractical">{{ showPractical ? 'Hide Practical' : 'Show Practical' }}</button>
+</template>
---
+++ vue/lifecycle/src/components/Basic.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { ref, onBeforeMount, onMounted, onUpdated, onBeforeUpdate, onUnmounted, onBeforeUnmount } from 'vue'
+
+console.log('On setup')
+
+onMounted(() => {
+  console.log('On mounted')
+})
+
+onBeforeMount(() => {
+  console.log('On before mount')
+})
+
+onUpdated(() => {
+  console.log('On updated')
+})
+
+onBeforeUpdate(() => {
+  console.log('On before update')
+})
+
+onUnmounted(() => {
+  console.log('On unmounted')
+})
+
+onBeforeUnmount(() => {
+  console.log('On before unmount')
+})
+
+const currentTime = ref(new Date())
+let timer: ReturnType<typeof setInterval> | null = null
+
+function onButtonTimeClick() {
+  if (timer) {
+    clearInterval(timer)
+    timer = null
+  } else {
+    timer = setInterval(() => {
+      currentTime.value = new Date()
+    }, 1000)
+  }
+}
+</script>
+
+<template>
+  <h2>Basic</h2>
+  <p>Current time: {{ currentTime }}</p>
+  <button @click="onButtonTimeClick" type="button">{{ timer ? 'Stop' : 'Start' }}</button>
+</template>
