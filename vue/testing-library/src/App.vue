425512d6ff5e71c1c6aff9f6599884bef90877eb
Nakorn Sinpadung <nakorndev@gmail.com>
2025-03-13 10:45:48 +0700

    feat: add initial setup for Vue testing library with MyButton component and tests

---
+++ vue/testing-library/src/App.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import MyButton from './components/MyButton.vue';
+
+function onClick() {
+  alert('Clicked!')
+}
+</script>
+
+<template>
+  <div class="max-w-md p-4">
+    <h2>MyButton.vue</h2>
+
+    <div class="flex flex-wrap items-center gap-1">
+      <MyButton></MyButton>
+      <MyButton size="sm">sm</MyButton>
+      <MyButton size="md">md</MyButton>
+      <MyButton size="lg">lg</MyButton>
+      <MyButton color="amber">amber</MyButton>
+      <MyButton color="blue">blue</MyButton>
+      <MyButton color="red">red</MyButton>
+      <MyButton color="gray">gray</MyButton>
+      <MyButton color="black">black</MyButton>
+      <MyButton disabled>disabled</MyButton>
+      <MyButton loading>loading</MyButton>
+      <MyButton @click="onClick">v-on:click</MyButton>
+    </div>
+
+    <hr>
+
+    <h2>TodoList.vue</h2>
+
+    <hr>
+
+    <h2>CreateTodo.vue</h2>
+  </div>
+</template>
