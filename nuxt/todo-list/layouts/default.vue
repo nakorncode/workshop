73d70498164efc1fc31e7bcca2f770cb4e61fa0f
Nakorn Sinpadung <nakorndev@gmail.com>
2025-03-27 15:39:34 +0700

    feat: add login page and update navigation links for authentication

--- nuxt/todo-list/layouts/default.vue
+++ nuxt/todo-list/layouts/default.vue
@@ -4,7 +4,8 @@
       <div class="max-w-md mx-auto bg-gray-100 p-2 rounded-md shadow">
         <ul class="flex items-center gap-2">
           <li><NuxtLink class="font-bold text-lg text-blue-600 underline" to="/">Todo List</NuxtLink></li>
-          <li class="ml-auto"><NuxtLink to="/sign-up" class="text-white bg-blue-600 px-2 py-1 rounded-md hover:bg-blue-800">Sign Up</NuxtLink></li>
+          <li class="ml-auto"><NuxtLink to="/login" class="text-blue-600 underline">Login</NuxtLink></li>
+          <li><NuxtLink to="/sign-up" class="text-white bg-blue-600 px-2 py-1 rounded-md hover:bg-blue-800">Sign Up</NuxtLink></li>
         </ul>
       </div>
     </nav>
---
+++ nuxt/todo-list/pages/login.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+const input = ref({
+  email: '',
+  password: ''
+})
+
+const toast = useToast()
+
+async function onLogin() {
+  const { data, error } = await authClient.signIn.email({
+    ...input.value
+  })
+  if (error) {
+    toast.add({ title: error.message || 'Unknown error', color: 'error' })
+    return
+  }
+  toast.add({ title: `Login successful on email ${data.user.email}`, color: 'success' })
+  await navigateTo('/')
+}
+</script>
+
+<template>
+  <div class="max-w-xs mx-auto">
+    <h1 class="font-bold text-2xl mb-4">Login</h1>
+    <form @submit.prevent="onLogin">
+      <div class="flex flex-col gap-1">
+        <UFormField label="Email">
+          <UInput v-model="input.email" name="email" class="w-full" placeholder="Email" type="email"/>
+        </UFormField>
+        <UFormField label="Password">
+          <UInput v-model="input.password" name="password" class="w-full" placeholder="Password" type="password"/>
+        </UFormField>
+        <div class="mt-3">
+          <UButton type="submit" block>Submit</UButton>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
