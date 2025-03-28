015c3ad77b930dada3fd31bc34ce09e7dae0a312
Nakorn Sinpadung <nakorndev@gmail.com>
2025-03-28 09:48:43 +0700

    feat: set application title for Nuxt Todo List

--- nuxt/todo-list/nuxt.config.ts
+++ nuxt/todo-list/nuxt.config.ts
@@ -5,6 +5,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      title: 'Nuxt Todo List',
+    }
+  },
   vite: {
     plugins: [
       tailwindcss(),
