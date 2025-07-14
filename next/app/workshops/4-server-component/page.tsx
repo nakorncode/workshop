673eaa2861b97b3ce5acac10ea982f39ebd42119
Nakorn Sinpadung <nakorndev@gmail.com>
2025-07-14 11:43:32 +0700

    fix: correct typo in comment regarding React Hooks usage in server component

--- next/app/workshops/4-server-component/page.tsx
+++ next/app/workshops/4-server-component/page.tsx
@@ -1,6 +1,6 @@
 export default async function Page() {
   const product = await fetch('https://dummyjson.com/products/1?select=title,price').then(res => res.json()) as { title: string, price: number }
-  // จะไม่สามารถใช้งาน Reach Hooks ต่างๆได้ นอกจากจะเป็น Client Component
+  // จะไม่สามารถใช้งาน React Hooks ต่างๆได้ นอกจากจะเป็น Client Component
   return (
     <>
       <h1 className="text-xl font-bold mb-2">Server Component</h1>
