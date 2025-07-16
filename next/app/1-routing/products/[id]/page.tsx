abc85b87b94cab1efbdb6a496d2028a6e15b6be7
Nakorn Sinpadung <nakorndev@gmail.com>
2025-07-16 15:26:46 +0700

    fix: correct return type of generateMetadata function to Promise<Metadata>

--- next/app/1-routing/products/[id]/page.tsx
+++ next/app/1-routing/products/[id]/page.tsx
@@ -3,7 +3,7 @@ import { getProduct } from "../../lib/products"
 import { Metadata } from "next"
 
 // คล้ายกับ export const metadata = {} แต่ไว้ใช้สำหรับ async function สำหรับ Dynamic Route โดยเฉพาะ
-export async function generateMetadata(props: { params: Promise<{ id: string }> }): Metadata {
+export async function generateMetadata(props: { params: Promise<{ id: string }> }): Promise<Metadata> {
   const params = await props.params
   const id = parseInt(params.id)
   if (isNaN(id)) {
