6b2602cd4625a91f624026b688410024cf318dea
Nakorn Sinpadung <nakorndev@gmail.com>
2025-07-16 21:59:58 +0700

    feat: add dynamic and static rendering pages with user data fetching

--- next/app/3-rendering/page.tsx
+++ next/app/3-rendering/page.tsx
@@ -5,6 +5,8 @@ export default function Page() {
       <ul className="pl-5 list-disc space-y-1">
         <li><a href="/3-rendering/hydration-error" className="text-blue-600 underline">Hydration Error</a></li>
         <li><a href="/3-rendering/env" className="text-blue-600 underline">Environment Variables</a></li>
+        <li><a href="/3-rendering/static" className="text-blue-600 underline">Static Rendering</a></li>
+        <li><a href="/3-rendering/dynamic" className="text-blue-600 underline">Dynamic Rendering</a></li>
       </ul>
     </div>
   )
