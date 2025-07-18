d096a58cfd485c2bea0448917c3f487875f38094
Nakorn Sinpadung <nakorndev@gmail.com>
2025-07-18 07:55:45 +0700

    feat: create client functions page with hooks demonstration and redirect functionality

--- next/app/4-functions/page.tsx
+++ next/app/4-functions/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
     <div>
       <h1 className="text-xl font-bold mb-3">Functions</h1>
       <ul>
-        <li><a href="/4-functions/client" className="underline text-blue-600">Client Functions</a></li>
+        <li><a href="/4-functions/client/some-slug?param1=value1&param2=value2" className="underline text-blue-600">Client Functions</a></li>
         <li><a href="/4-functions/server" className="underline text-blue-600">Server Functions</a></li>
       </ul>
     </div>
