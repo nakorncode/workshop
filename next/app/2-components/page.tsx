436114214e4a722d074ae852c2efa4e95b57bcf3
Nakorn Sinpadung <nakorndev@gmail.com>
2025-07-21 11:31:59 +0700

    feat: add Counter component and integrate it into the mixed components page

--- next/app/2-components/page.tsx
+++ next/app/2-components/page.tsx
@@ -5,6 +5,7 @@ export default function Page() {
       <ul className="pl-5 list-disc space-y-1">
         <li><a href="/2-components/client" className="text-blue-600 underline">Client Components</a></li>
         <li><a href="/2-components/server" className="text-blue-600 underline">Server Components</a></li>
+        <li><a href="/2-components/mixed" className="text-blue-600 underline">Mixed Components</a></li>
       </ul>
     </div>
   )
