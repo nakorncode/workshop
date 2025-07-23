ff2ab5501d71ca542b809f8b71efcc1249d7b999
Nakorn Sinpadung <nakorndev@gmail.com>
2025-07-23 11:21:26 +0700

    feat: add Timer component for time tracking functionality

--- svelte/src/routes/2-effect/+page.svelte
+++ svelte/src/routes/2-effect/+page.svelte
@@ -1,9 +1,14 @@
 <script>
   import SalesData from "./SalesData.svelte"
   import Note from "./Note.svelte"
+  import Timer from "./Timer.svelte";
 </script>
 
 <Note></Note>
+
+<hr class="my-4">
+
+<Timer></Timer>
 
 <hr class="my-4">
 
