2b3e7e32116fe8edabafeb14541af41d7509ec73
Nakorn Sinpadung <nakorndev@gmail.com>
2025-07-23 14:17:30 +0700

    feat: update link label and add 5-blocks component for age and product handling

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -8,12 +8,12 @@
     { href: '/2-effect', label: '$effect()' },
     { href: '/3-derived', label: '$derived()' },
     { href: '/4-props', label: '$props()' },
-    { href: '/5-control-flow', label: 'Control Flow' }
+    { href: '/5-blocks', label: 'Blocks {#...}' }
   ]
 </script>
 
 <h1 class="text-2xl font-bold mb-3">Svelte Workshop by NakornCode</h1>
-<ol class="list-decimal pl-6 space-y-1 text-lg">
+<ol class="list-decimal pl-8 space-y-1 text-lg font-mono">
   {#each links as link, index (index)}
     <li>
       <a href={link.href} class="text-blue-600 underline">{link.label}</a>
