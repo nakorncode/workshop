292e7c0301ba5461684b8f96d4198efe75463716
Nakorn Sinpadung <nakorndev@gmail.com>
2025-07-26 11:24:42 +0700

    feat: add layouts and nested routes for 10-layouts section

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -13,11 +13,12 @@
     { href: '/7-directives', label: 'Directives ...:' },
     { href: '/8-advanced-components', label: 'Advanced Components' },
     { href: '/9-routing', label: 'Routing' },
+    { href: '/10-layouts', label: 'Layouts' },
   ]
 </script>
 
 <h1 class="text-2xl font-bold mb-3">Svelte Workshop by NakornCode</h1>
-<ol class="list-decimal pl-8 space-y-1 text-lg font-mono">
+<ol class="list-decimal pl-10 space-y-1 text-lg font-mono">
   {#each links as link, index (index)}
     <li>
       <a href={link.href} class="text-blue-600 underline">{link.label}</a>
